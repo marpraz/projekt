f8dc16c508448bf18c00575ea2a3522d0c585b7c
marpraz <martinaprazakova.hk@seznam.cz>
2025-04-30 23:59:50 +0200

    posledni

--- fairytale_generator/lib/main.dart
+++ fairytale_generator/lib/main.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart'; 
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'screens/home_screen.dart';
 
 Future<void> main() async {
-  await dotenv.load(); 
+  await dotenv.load(fileName: '.env');
   runApp(const FairyTaleApp());
 }
 
